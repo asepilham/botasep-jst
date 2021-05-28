14945be0325f750c1bbe1e609d887f6123405cf1
asepilham <84943488+asepilham@users.noreply.github.com>
2021-05-28 21:48:36 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,43 +15,18 @@ bot.onText(/\/start/, (msg) => {
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        'hello ${msg.chat.first_name}, welcome...\n
-        click /predict'
+        `hello ${msg.chat.first_name}, welcome...\n
+        click /menu to main menu`
     );   
-});
-state = 0;
-bot.onText(/\/predict/, (msg) => { 
-    bot.sendMessage(
-        msg.chat.id,
-        'masukan nilai i|v contohnya 9|9'
-    );   
-    state =1;
 });
 
-bot.on('message',(msg)=> {
-    if(state == 1){
-        s = msg.text.split("|");
-        i = s[0]
-        v = s[1]
-        model.predict(
-            [
-                parseFloat(s[0]),
-                parseFloat(s[1])
-            ]
-        ).then((jres)=>{
-            bot.sendMessage(
-                msg.chat.id,
-                'nilai v yang diprediksi adalah ${jres[0]) volt'
-            );
-            bot.sendMessage(
-                msg.chat.id,
-                'nilai p yang diprediksi adalah ${jres[1]) watt'
-            );
-        })
-    }else{
-        state = 0
-    }
-})
+bot.onText(/\/menu/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `this is your main menu`
+    );   
+});
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
